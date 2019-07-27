545717a77d45beb4be27aaada2808251602b0685
ashah88 <53231552+ashah88@users.noreply.github.com>
2019-07-27 15:09:13 -0700

    Update ajay.php

--- ajay.php
+++ ajay.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 </head>
-<body class="bg-dark">
+<body class="bg-primary">
 <div class="container">
 
 <h1 class="text-secondary">Thoughts</h1>
